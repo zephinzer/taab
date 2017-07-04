defa49da7a72c2f5dd978c567c1a667bda7948b8
zephinzer <dev@joeir.net>
2017-07-04 09:36:33 +0800

    added basic test for props

---
+++ test/index.test.js
@@ -0,0 +1,32 @@
+const TAAB = require('../');
+
+describe('taab', () => {
+  context('static props', () => {
+    it('has the correct static properties/methods', () => {
+      const taabStaticProperties = Object.keys(TAAB);
+      expect(taabStaticProperties).to.deep.equal([
+        'constant',
+        'init',
+      ]);
+    });
+  });
+
+  context('instance props', () => {
+    const taabInstance = new TAAB();
+
+    it('has the correct instance properties/methods', () => {
+      const taabInstanceProperties = Object.getPrototypeOf(taabInstance);
+      expect(taabInstanceProperties).to.contain.keys([
+        'createBoard',
+        'createList',
+        'createCard',
+        'getBoards',
+        'getCards',
+        'getMember',
+        'getOrganisations',
+        'getOrganizations',
+        'getProfile',
+      ]);
+    });
+  });
+});
