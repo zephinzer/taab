44c3450f105de7bef1c00b64fe733e977bb8463a
zephinzer <dev@joeir.net>
2017-07-10 01:08:20 +0800

    added defaults for card into const

--- test/lib/card.test.js
+++ test/lib/card.test.js
@@ -1,3 +1,4 @@
+const taabConst = require('../../lib/const');
 const rewire = require('rewire');
 const taabCard = rewire('../../lib/card');
 
@@ -5,8 +6,8 @@ const createApiHandlerSpy = sinon.spy();
 const apiHandler = sinon.spy();
 
 const mockTaabUtils = {
-  createApiHandler: (method, uri) => {
-    createApiHandlerSpy(method, uri);
+  createApiHandler: (method, uri, params) => {
+    createApiHandlerSpy(method, uri, params);
     return apiHandler;
   },
 };
@@ -25,13 +26,19 @@ describe('taab card', () => {
       }).to.throw();
     });
 
+    it('throws an error if the :idList argument is not specified', () => {
+      expect(() => {
+        taabCard.create({});
+      }).to.throw();
+    });
+
     it('calls the standard api handler creator', () => {
-      taabCard.create({});
+      taabCard.create({idList: ''});
       expect(apiHandler).to.be.calledOnce;
     });
 
     it('calls the correct trello endpoint', () => {
-      taabCard.create({});
+      taabCard.create({idList: ''});
       expect(createApiHandlerSpy).to.be.calledWith('post', '/cards');
     });
   });
@@ -42,14 +49,34 @@ describe('taab card', () => {
       createApiHandlerSpy.reset();
     });
 
+    it('throws an error if no arguments are specified', () => {
+      expect(() => {
+        taabCard.query();
+      }).to.throw();
+    });
+
     it('calls the standard api handler creator', () => {
-      taabCard.query();
+      taabCard.query({});
       expect(apiHandler).to.be.calledOnce;
     });
 
     it('calls the correct trello endpoint', () => {
-      taabCard.query();
-      expect(createApiHandlerSpy).to.be.calledWith('get', '/members/me/cards');
+      taabCard.query({});
+      expect(createApiHandlerSpy).to.be.calledWith('get', '/members/me/cards', {
+        actions: taabConst.defaults.cardActions,
+        attachments: taabConst.defaults.cardAttachments,
+        attachment_fields: taabConst.defaults.cardAttachmentFields, // eslint-disable-line camelcase
+        stickers: taabConst.defaults.cardStickers,
+        members: taabConst.defaults.cardMembers,
+        member_fields: taabConst.defaults.cardMemberFields, // eslint-disable-line camelcase
+        checkItemStates: taabConst.defaults.cardCheckItemStates,
+        checklists: taabConst.defaults.cardChecklists,
+        limit: taabConst.defaults.cardLimit,
+        since: taabConst.defaults.cardSince,
+        before: taabConst.defaults.cardBefore,
+        filter: taabConst.defaults.cardFilter,
+        fields: taabConst.defaults.cardFields,
+      });
     });
   });
 });
